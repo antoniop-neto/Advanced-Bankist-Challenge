e981f0e3aa763a233d326ebcc1163665dc2c0b64
Antonio Oliveira <antoniop_neto@hotmail.com>
2024-05-15 08:54:33 +0100

    last adjust on the time to load the sections

--- script.js
+++ script.js
@@ -202,7 +202,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(section => {
   sectionObserver.observe(section);
-  // section.classList.add('section--hidden');
+  section.classList.add('section--hidden');
 });
 
 ///////////////////////////////////////////////////////////////
